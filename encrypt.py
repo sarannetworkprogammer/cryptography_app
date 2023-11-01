fcec168b7976a1dff0a4ffd24d64519952bd691b
Saran <46473540+sarannetworkprogammer@users.noreply.github.com>
2023-11-01 12:32:17 -0500

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 
 
-st.header("Cryptography")
+st.markdown("<h1 style='text-align: center; color: Blue;'>Cryptography</h1>", unsafe_allow_html=True)
 
 st.subheader("Encrypt Functionality")
 
